dc12e1647f006224f73b93f126d9e482902f0b20
Madhumitha V <madhumitha@browserstack.com>
2024-06-08 19:29:43 +0530

    Analysing greed game

--- koans/about_scoring_project.rb
+++ koans/about_scoring_project.rb
@@ -46,18 +46,20 @@ class AboutScoringProject < Neo::Koan
     assert_equal 100, score([1])
   end
 
+  # 1 - 100, 5 - 50
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
     assert_equal 300, score([1,5,5,1])
   end
 
+  #  2 | 3 | 4| 6 = 0
   def test_score_of_single_2s_3s_4s_and_6s_are_zero
     assert_equal 0, score([2,3,4,6])
   end
-
+#  1,1,1 - 1000
   def test_score_of_a_triple_1_is_1000
     assert_equal 1000, score([1,1,1])
   end
-
+# 222 | 333| 444| 555| 666 - 100x
   def test_score_of_other_triples_is_100x
     assert_equal 200, score([2,2,2])
     assert_equal 300, score([3,3,3])
